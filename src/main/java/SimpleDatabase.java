de5d89453f9ac365c2700362c3bfc4385dce0c22
Eric Siebeneich <eric.siebeneich@exclusr.com>
2015-02-17 11:48:36 -0600

    Added launcher class

---
+++ src/main/java/SimpleDatabase.java
@@ -0,0 +1,14 @@
+/**
+ * User: Eric
+ * Date: 2/17/15
+ */
+public class SimpleDatabase {
+
+	public SimpleDatabase() {
+
+	}
+
+	public static void main(String[] args) {
+		new SimpleDatabase();
+	}
+}
